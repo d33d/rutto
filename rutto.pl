83da20dfe6f3fb9c27d4d0f7486356cd7493b146
nemux <marco.romano@airtelco.com>
2016-01-26 16:53:07 +0100

    something works
    first commit that works… just to dump traffic

--- rutto.pl
+++ rutto.pl
@@ -26,67 +26,116 @@ Mojo::IOLoop->server(
         # Read connect request from client
         my $buffer = $buffer{$client}{client} .= $chunk;
         
-        #say $buffer;
+        if ($buffer =~ /\x0d?\x0a\x0d?\x0a$/) {
+          $buffer{$client}{client} = '';
 
-        my $req = Mojo::Message::Request->new;
-        $req->parse($buffer);
+          #### TLS
+          if ($buffer =~ /CONNECT (\S+):(\d+)?/) {
+            my $address = $1;
+            my $port = $2 || 80;
 
-        my $host = $req->headers->host;
-        #say $host;
+            say "TLS";
 
-        #say $req->method;
-        #say $req->headers->host;
-        #say $req->body;
-  
-        my ($address, $port) = split (':', $host);
-        $port = 80 if (!defined $port || $port !~ m/^\d+$/);
-        
-        $buffer{$client}{host} = $host;
-        $buffer{$client}{port} = $port;
-          
-        # Connection to server
-        $buffer{$client}{connection} = Mojo::IOLoop->client(
-          {address => $address, port => $port} => sub {
-            my ($loop, $err, $stream) = @_;
- 
-            # Connection to server failed
-            if ($err) {
-              say "Connection error for $address:$port: $err" if ($verbose);
-              Mojo::IOLoop->remove($client);
-              return delete $buffer{$client};
-            }
- 
-            # Start forwarding data in both directions
-            say "\n\n[****] CLIENT >>>>> SERVER $address:$port";
-            $stream->write($req->to_string);
+            # Connection to server
+            $buffer{$client}{connection} = Mojo::IOLoop->client(
+              {address => $address, port => $port} => sub {
+                my ($loop, $err, $stream) = @_;
 
-            ### TLS with CONNECT request....
-            #Mojo::IOLoop->stream($client)->write("HTTP/1.1 200 OK\x0d\x0a" . "Connection: keep-alive\x0d\x0a\x0d\x0a");
-            #Mojo::IOLoop->stream($server)->write($req->to_string);
-            
-            $stream->on(
-              read => sub {
-                my ($stream, $chunk) = @_;
-                if ($verbose) {
-                  say "[****] SERVER >>>>> CLIENT";
-                  say $chunk;
+                # Connection to server failed
+                if ($err) {
+                  say "Connection error for $address:$port: $err";
+                  Mojo::IOLoop->remove($client);
+                  return delete $buffer{$client};
                 }
-                Mojo::IOLoop->stream($client)->write($chunk);
-              }
-            );
- 
-            # Server closed connection
-            $stream->on(
-              close => sub {
-                #say "CLOSE CLIENT: " . $client;
-                Mojo::IOLoop->remove($client);
-                delete $buffer{$client};
+
+                # Start forwarding data in both directions
+                say "Forwarding to $address:$port";
+                Mojo::IOLoop->stream($client)
+                  ->write("HTTP/1.1 200 OK\x0d\x0a"
+                    . "Connection: keep-alive\x0d\x0a\x0d\x0a");
+                $stream->on(
+                  read => sub {
+                    my ($stream, $chunk) = @_;
+                    Mojo::IOLoop->stream($client)->write($chunk);
+                  }
+                );
+
+                # Server closed connection
+                $stream->on(
+                  close => sub {
+                    Mojo::IOLoop->remove($client);
+                    delete $buffer{$client};
+                  }
+                );
               }
             );
           }
-        );
+          
+          else {  
 
+            my $req = Mojo::Message::Request->new;
+            $req->parse($buffer);
+
+            my $host = $req->headers->host;
+            #say $host;
+
+            #say $req->method;
+            #say $req->headers->host;
+            #say $req->body;
+      
+            my ($address, $port) = split (':', $host);
+            $port = 80 if (!defined $port || $port !~ m/^\d+$/);
+            
+            $buffer{$client}{host} = $host;
+            $buffer{$client}{port} = $port;
+              
+            # Connection to server
+            $buffer{$client}{connection} = Mojo::IOLoop->client(
+              {address => $address, port => $port} => sub {
+                my ($loop, $err, $stream) = @_;
+     
+                # Connection to server failed
+                if ($err) {
+                  say "Connection error for $address:$port: $err" if ($verbose);
+                  Mojo::IOLoop->remove($client);
+                  return delete $buffer{$client};
+                }
+     
+                # Start forwarding data in both directions
+                say "\n\n[****] CLIENT >>>>> SERVER $address:$port";
+                $stream->write($req->to_string);
+
+                ### TLS with CONNECT request....
+                #Mojo::IOLoop->stream($client)->write("HTTP/1.1 200 OK\x0d\x0a" . "Connection: keep-alive\x0d\x0a\x0d\x0a");
+                #Mojo::IOLoop->stream($server)->write($req->to_string);
+                
+                $stream->on(
+                  read => sub {
+                    my ($stream, $chunk) = @_;
+                    if ($verbose) {
+                      say "[****] SERVER >>>>> CLIENT";
+                      say $chunk;
+                    }
+                    Mojo::IOLoop->stream($client)->write($chunk);
+                  }
+                );
+     
+                # Server closed connection
+                $stream->on(
+                  close => sub {
+                    #say "CLOSE CLIENT: " . $client;
+                    Mojo::IOLoop->remove($client);
+                    delete $buffer{$client};
+                  }
+                );
+              }
+            );
+
+          }
+        }
+        
       }
+
     );
 
     # Client closed connection
@@ -102,8 +151,7 @@ Mojo::IOLoop->server(
 );
  
 print <<'EOF';
-Starting CONNECT proxy on port 3000.
-Now use RUTTO as proxy "HTTP_PROXY=http://127.0.0.1:3000".
+Now your RUTTO Proxy is on 127.0.0.1 port 3000.
 EOF
  
 Mojo::IOLoop->start;
